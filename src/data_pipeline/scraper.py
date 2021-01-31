4ec4466bda4dc9b58c4640fe6c2a36bd54594fd8
OliverOA <OliverAOakman@gmail.com>
2021-01-31 22:47:24 +1100

    Ready to scrape the website

--- src/data_pipeline/scraper.py
+++ src/data_pipeline/scraper.py
@@ -3,8 +3,11 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-
-import_url = "http://www.boardspace.net/hive/hivegames/"
+from zipfile import ZipFile
+import os
+import shutil
+import sys
+import time
 
 
 def get_file_links(url_in):
@@ -13,9 +16,9 @@ def get_file_links(url_in):
     soup = BeautifulSoup(req.text, "html.parser")
     links = soup.find_all("a")
 
-    zips = []
-    sgfs = []
-    expansion = []
+    zips = []  # Collect .zip files
+    sgfs = []  # Collect .sgf files (native)
+    expansion = []  # A spot for sub-directories, allows recursion
 
     for link in links:
         href_code = link.get("href")  # Get the href
@@ -26,9 +29,9 @@ def get_file_links(url_in):
             pass
         # If there are zips or sgfs in the directory...
         elif href_code.endswith(".zip"):
-            zips.append(url_in + link)
+            zips.append(url_in + href_code)
         elif href_code.endswith(".sgf"):
-            sgfs.append(url_in + link)
+            sgfs.append(url_in + href_code)
         # Finally, if there are any other valid directories to search...
         elif href_code.startswith("archive") or href_code.startswith("games"):
             expansion.append(href_code)
@@ -36,38 +39,59 @@ def get_file_links(url_in):
     for url in expansion:
         # For each url, conduct the same method...
         new_url = url_in + url
-        print("Conducting recursion on " + new_url)
-        new_zips, new_sgfs, new_expansion = get_file_links(new_url)
+        print("Scraping sub-directory " + new_url)
+        new_zips, new_sgfs, new_expansion = get_file_links(new_url)  # Recursively run the function
         zips.extend(new_zips)
         sgfs.extend(new_sgfs)
 
     return zips, sgfs, expansion
 
 
-#def downloader(url):
-#    # ADD EXISTENCE CHECK
+def downloader(url, dl_path, extract_path, full_download=False):
+    # Prepare the file for manipulation
+    file_ext = url[-4:]
+    str_split = url.split('/')
+    filename = str_split[-1]
 
+    if os.path.isfile(dl_path + "/" + filename) and not full_download:
+        print("Skipping download of " + filename)  # Mechanism to skip downloads
+        return None
+    else:
+        print("Downloading " + filename)
+        with open(dl_path + filename, "wb") as f:
+            req = requests.get(url, stream=True)
+            total_length = req.headers.get("content-length")
 
+            if total_length is None:
+                f.write(req.content)
+            else:
+                dl = 0
+                total_length = int(total_length)
+                for data in req.iter_content(chunk_size=4096):
+                    dl += len(data)
+                    f.write(data)
+                    done = int(50 * dl/total_length)
+                    sys.stdout.write("\r[%s%s]" % ("=" * done, " " * (50-done)))
+                    sys.stdout.flush()
+        print("\rPausing for 2 seconds...")
+        time.sleep(2)  # Force a 2 second delay to not kill the website
 
+    if file_ext == ".sgf":
+        print("\rCopying " + filename)
+        shutil.copy(dl_path + filename, extract_path)
+    elif file_ext == ".zip":
+        print("\rUnzipping " + filename)
+        with ZipFile(dl_path + filename, "r") as zipObj:
+            zipObj.extractall(extract_path)
+    return None
+
+# Script test
+import_url = "http://www.boardspace.net/hive/hivegames/"
 zips, sgfs, expansion = get_file_links(import_url)
 
-print(zips)
+download_list = zips + sgfs
+download_path = "../data/download_raw/"
+game_path = "../data/download_games/"
 
-
-
-
-#def get_fn(dir):
-#    if not dir:
-#        return None
-#    fname = re.findall("filename=(.+)", dir)
-#    if len(fname) == 0:
-#        return None
-#    return fname[0]
-#
-#import_url = "http://www.boardspace.net/hive/hivegames/"
-#download_path = "../data/"
-#
-#req = requests.get(import_url, allow_redirects=True)
-#filename = download_path
-#with open(filename, "wb") as f:
-#    f.write(req.content)
+for download_req in download_list:
+    downloader(download_req, download_path, game_path)
